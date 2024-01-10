fdd77ea600c7d1efdea5c421559238c27fe49b0f
aleeteo <aleteo946@gmail.com>
2024-01-10 16:14:55 +0100

    rifatto da zero ool.lisp

--- OOΛ/OOΛ.lisp
+++ OOΛ/OOΛ.lisp
@@ -1,90 +1,57 @@
-;;; -*- Mode: Lisp -*-
-;;; OOΛ.lisp
+(defparameter classes-specs (make-hash-table))
 
-(defparameter central-memory (make-hash-table))
+(defun add-class-spec (name class-spec)
+  (setf (gethash name classes-specs) class-spec))
 
-;;; def-class definisce la struttura di una classe e la memorizza
-;;; in una locazione centralizzata (una variabile globale).
-(defun def-class (class-name parents &rest parts)
-  (cond ((and (symbolp class-name)
-              (listp parents))
-         (let (table (make-table parts))
-           (setf (gethash class-name central-memory)
-                 (cons parents table)))
-         class-name)
-        (t (error "Errore: classe invalida"))))
+(defun class-spec (name)
+  (gethash name classes-specs))
 
-;;; make: crea una nuova istanza di una classe.
-(defun make (class-name &rest parts)
-  (cond (and (symbolp class-name)
-             ) ;TODO controllo sul secondo argomento
-                                        ;TODO creazione istanza
-        (t (error "Error: invalid instance"))))
 
-;;; is-class: restituisce T se l’atomo passatogli `e il nome di una classe.
-(defun is-class (class-name)
-  (cond ((symbolp class-name)
-         ) ;TODO controllo classe
-        (t (error "invalid class name"))))
+(defun def-class (class-name parents &rest part)
+  (cond
+    ((and (symbolp class-name) (listp parents))
+     (let ((fields (get-fields (assoc 'fields part)))
+           (methods (get-methods (assoc 'methods part))))
+       (add-class-spec class-name (list
+                                   (cons 'name class-name)
+                                   (cons 'parents parents)
+                                   (cons 'fields fields)
+                                   (cons 'methods methods))))
+     class-name)
+    (t (error "ERROR: class-name or parents not valid!"))))
 
-;;; is-instance: restituisce T se l’oggetto passatogli `e l’istanza
-;;; di una classe.
-(defun is-instance (&optional class-name value))
 
-;;; field: estrae il valore di un campo da una classe.
-(defun field (instance field-name))
+(defun get-fields (field-part)
+  (cond
+    ((null field-part) NIL)
+    (t (mapcar (lambda (field)
+                 (let (
+                       (field-name (first field))
+                       (field-value (second field))
+                       (field-type (if (not (null (third field)))
+                                       (third field)
+                                       NIL)))
+                   (list :name field-name
+                         :value field-value
+                         :type field-type)))
+               (cdr field-part)))))
 
-;;; field*: estrae il valore da una classe percorrendo una catena di attributi.
-(defun field* (instance field-name))
-;;; end od file -- OOΛ.lisp
+(defun get-methods (method-part)
+  (cond
+    ((null method-part) NIL)
+    (t (mapcar (lambda (method)
+                 (cond
+                   ((not (valid-method-structure method))
+                    (error "ERROR: invalid method structure ~a" method))
+                   ((not (symbolp (first method)))
+                    (error "ERROR: invalid method name ~a" method))
+                   (t (let ((method-name (first method))
+                            (method-body (rest method)))
+                        (list :name method-name
+                              :body (eval method-body))))))
+               (cdr method-part)))))
 
-;; (defun make-table (list)
-;;   (cond ((null (list)) nil)
-;;         ((not (symbolp (first list)))
-;;          (error "Errore: la chiave non è un simbolo"))
-;;         ((null (second list))
-;;          (error "Errore: nessun valore da abbinare alla chiave"))
-;;         (t
-;;          (if (is-method (second (list)))
-;;              (append (list
-;;                       (cons (first list)
-;;                             (proc-method (first list)
-;;                                          (second list))))
-;;                      (make-table (cddr (list))))
-;;              (append (list
-;;                       (cons (first list)
-;;                             (second list)))
-;;                      (make-table (cddr (list))))))))
-
-;; (defun make-table (parts)
-;;   (cond ((null parts) nil)
-;;         ((not (listp (car parts))) nil)
-;;         ((eql (caar parts) 'fields)
-;;          (cons (make-table-fields (cdr (car parts)))
-;;                (make-table (cdr parts))))
-;;         ((eql (caar parts) 'methods)
-;;          (cons (make-table (cdr parts))
-;;                (make-table-methods (cdr (car parts)))))
-;;         (t )))
-
-;;; is-method: controlla se una lista è un metodo (in part methods)
-(defun is-method (method*)
-  (and (listp method*)
-       (symbolp (first method*))
-       (listp (second method*))
-       (listp (third method*))))
-
-<<<<<<< HEAD
-=======
-;;; is-class: verifica dato il nome di una classe se essa è un simbolo e se ha una hash table associata ad essa
-(defun is-class (class-name)
-    (and
-        (symbolp class-name)
-        (hash-table-p (symbol-value class-name))))
-
->>>>>>> origin/main
-                                        ;DONE create-assoc function
-                                        ;DONE is-method function
-                                        ;DONE proc-method function
-                                        ;TODO part-exist function
-                                        ;TODO get-part function
+(defun valid-method-structure (method)
+  (and (listp method)
+       (= (length method) 2)
+       (listp (second method))))
