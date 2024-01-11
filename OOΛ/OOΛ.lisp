7b44e53afb6b7d0aceb500e38a5af2f74d03e404
aleeteo <aleteo946@gmail.com>
2024-01-11 10:01:00 +0100

    make function and subfunctions with test

--- OOΛ/OOΛ.lisp
+++ OOΛ/OOΛ.lisp
@@ -55,3 +55,41 @@
   (and (listp method)
        (= (length method) 2)
        (listp (second method))))
+
+;;; make consente di creare un'istanza di una classe
+(defun make (class-name &rest parts)
+  (cond
+    ((is-class class-name)
+     (let ((class-specs (class-spec class-name)))
+       (print parts)
+       (if (verify-instance-fields class-specs parts)
+           (append (list 'oolinst)
+                   (list :class class-name :fields parts)))))
+    (t (error "error: given class not valid"))))
+
+;;; is-class verifica che il nome della classe
+;;; passata sia di una classe presente in memoria
+(defun is-class (class-name)
+  (and (symbolp class-name)
+       (gethash class-name classes-specs)))
+
+(defun verify-instance-fields (class-specs fields)
+  (cond
+    ((null fields) t) ;;da rimettere nil
+    ((not (null (cdr (third class-specs))))
+     (let ((class-fields (cdr (third class-specs))))
+       (if (deep-member (first fields) class-fields)
+           (verify-instance-fields class-specs (nthcdr 2 fields)))))
+    (t (error "error: no field in class"))))
+
+(defun deep-member (atomo lista)
+  (cond ((null lista) nil) ; caso base: lista vuota
+        ((eq atomo (car lista)) t) ; atomo trovato
+        ((listp (car lista)) (or (deep-member atomo (car lista)) ; ricerca ricorsiva nella sottolista
+                                 (deep-member atomo (cdr lista)))) ; continua nella lista principale
+        (t (deep-member atomo (cdr lista))))) ; continua nella lista principale
+
+;;tests
+(def-class 'person nil '(fields (name "Eve") (age 21 integer)))
+(defparameter adam (make 'person 'name "Adam"))
+(print adam)
